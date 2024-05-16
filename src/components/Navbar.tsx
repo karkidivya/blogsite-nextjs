ead6a8e265753e23b364321215693dec305a8969
Divya <karkidivya5@gmail.com>
2024-05-16 18:17:13 +0545

    minor changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,9 +11,9 @@ function Navbar() {
   return (
     <nav className="p-4 md:p-6 shadow-md bg-blue-900 text-white">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        <a href="/" className="text-xl font-bold mb-4 md:mb-0">
+      <Link href="/"><div className="text-xl font-bold mb-4 md:mb-0">
           Blogsite
-        </a>
+        </div></Link>
         
           <Link href="/signin">
             <Button className="w-full md:w-auto bg-slate-100 text-black" variant={'outline'}>Login</Button>
